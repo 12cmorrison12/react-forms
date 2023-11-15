efd6815718e3e0c3bf6c47a8867b1391720c2f46
Carver Morrison <12cmorrison12@gmail.com>
2023-11-15 04:33:04 -0700

    finished box list page

---
+++ react-forms/boxes-app/src/BoxList.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
+
+function BoxList() {
+    const [boxes, setBoxes] = useState([]);
+    const addBox = (boxObj) => {
+        setBoxes(boxes => [...boxes, boxObj]);
+    };
+
+    const remove = (id) => {
+        setBoxes(boxes => boxes.filter(box => box.id !== id));
+    }
+
+    const boxComponent = boxes.map(box => (
+        <Box 
+            key={box.id}
+            id={box.id}
+            width={box.width}
+            height={box.height}
+            handleRemove={remove}
+            backgroundColor={box.backgroundColor}
+        />
+    ));
+
+    return (
+        <div>
+            <NewBoxForm createBox={addBox} />
+            {boxComponent}
+        </div>
+    );
+}
+
+export default BoxList;
