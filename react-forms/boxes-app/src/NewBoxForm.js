3fd1aa8cec2c51f4d1a3cfaefcb738a61242a0a0
Carver Morrison <12cmorrison12@gmail.com>
2023-11-15 17:06:11 -0700

    finished new box form file

--- react-forms/boxes-app/src/NewBoxForm.js
+++ react-forms/boxes-app/src/NewBoxForm.js
@@ -7,4 +7,64 @@ function NewBoxForm({ createBox }) {
         width: "",
         backgroundColor: ""
     });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(formData => ({
+            ...formData,
+            [name]: value
+        }));
+    }
+
+    const getInput = (e) => {
+        e.preventDefeault();
+        makeInput({...formData, id: uuid() });
+        setFormData({ height: "", width: "", backgroundColor: ""});
+    }
+
+    return (
+        <div>
+            <form onSubmit={getInput}>
+                <div>
+                    <label htmlFor="height">Height</label>
+                    <input
+                        onChange={handleChange}
+                        type="text"
+                        id="height"
+                        value={formData.height}
+                    />
+                </div>
+                <div>
+                <label htmlFor="width">Width</label>
+                    <input
+                        onChange={handleChange}
+                        type="text"
+                        id="width"
+                        value={formData.width}
+                    />
+                </div>
+                <div>
+                <label htmlFor="height">Height</label>
+                    <input
+                        onChange={handleChange}
+                        type="text"
+                        id="height"
+                        value={formData.height}
+                    />
+                </div>
+                <div>
+                <label htmlFor="backgroundColor">Background Color</label>
+                    <input
+                        onChange={handleChange}
+                        type="text"
+                        id="backgroundColor"
+                        value={formData.backgroundColor}
+                    />
+                </div>
+                <button id="newBox">Add A Box</button>
+            </form>
+        </div>
+    );
 }
+
+export default NewBoxForm;
